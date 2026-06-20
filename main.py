0648e470716096a065f132a4a13cbce626762a43
igorgmnzmonte <igor.monte@academico.ifpb.edu.br>
2026-06-20 12:05:55 -0300

    feat: implementa funcao para buscar contatos na tabela do supabase

--- main.py
+++ main.py
@@ -22,3 +22,15 @@ ZAPI_TOKEN = os.getenv("ZAPI_TOKEN")
 if not all([SUPABASE_URL, SUPABASE_KEY, ZAPI_INSTANCE, ZAPI_TOKEN]):
     logging.error("Erro: Variáveis de ambiente ausentes no arquivo .env.")
     exit(1)
+
+def buscar_contatos() -> list:
+    # Busca os contatos cadastrados no Supabase limitando a 3 registros
+    try:
+        supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+        logging.info("Conectando ao Supabase para buscar contatos...")
+        
+        resposta = supabase.table("contatos").select("nome, telefone").limit(3).execute()
+        return resposta.data
+    except Exception as e:
+        logging.error(f"Erro ao buscar dados no Supabase: {e}")
+        return []
